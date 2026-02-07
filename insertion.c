bff04c0de9eb2ae576b1ed6ca5022e38564cb46d
Yue-nao <2683362602@qq.com>
2026-02-08 00:00:36 +0800

    insert a number into an array

--- insertion.c
+++ insertion.c
@@ -1,41 +1,52 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h> 
-int array_insert(int *x,int y,int Len){
+int array_insert(int *x,int y,int len)
+{
 	/*insert y into array x[]*/
-	int tmp=Len-1;
-	for(int i=0;i<Len-1;i++){
-		if(y<x[i]){
+	int tmp=len-1;
+	for(int i=0;i<len-1;i++)
+	{
+		if(y<x[i])
+		{
 			tmp=i;
 			break;
 		}
 	}
-	for(int i=Len-1;i>tmp;i--){
+	for(int i=len-1;i>tmp;i--)
+	{
 		x[i]=x[i-1];
 	}
 	x[tmp]=y; 
 	return 0;
 }
-int printf_array(int *x,int len){
-	for(int i=0;i<len;i++){
+int printf_array(int *x,int len)
+{
+	for(int i=0;i<len;i++)
+	{
 		printf("%d ",x[i]);
 	}
 	printf("\n");
 	return 0;
 }
-int main(int argc,char *argv[]){
+int main(int argc,char *argv[])
+{
 	int len=strlen(argv[1]);
 	int comma_count=0;
-	for(int i=0;i<len;++i){
-		if(argv[1][i]==','){
+	for(int i=0;i<len;++i)
+	{
+		if(argv[1][i]==',')
+		{
 			comma_count++;
 		}
 	}
 	int *a=(int*)malloc((comma_count+2)*4);
 	a[0]=atoi(&argv[1][0]);
 	int m=1;
-	for(int i=0;i<len;i++){
-		if(argv[1][i]==','){
+	for(int i=0;i<len;i++)
+	{
+		if(argv[1][i]==',')
+		{
 			a[m]=atoi(&argv[1][i+1]);
 			m++;
 		}
