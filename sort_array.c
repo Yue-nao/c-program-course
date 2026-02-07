e31c02919786a0a7ed4b0d22c7a117e133d1be43
Yue-nao <2683362602@qq.com>
2026-02-08 00:04:12 +0800

    sort an array

--- sort_array.c
+++ sort_array.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int array_sort(int *x,int len){
-	for(int i=0;i<len;i++){
-		for(int j=i+1;j<len;j++){
-			if(x[i]>x[j]){
+int array_sort(int *x,int len)
+{
+	for(int i=0;i<len;i++)
+	{
+		for(int j=i+1;j<len;j++)
+		{
+			if(x[i]>x[j])
+			{
 				int tmp=x[i];
 				x[i]=x[j];
 				x[j]=tmp;
@@ -13,26 +17,33 @@ int array_sort(int *x,int len){
 	}
 	return 0; 
 }
-int printf_array(int *x,int len){
-	for(int i=0;i<len;i++){
+int printf_array(int *x,int len)
+{
+	for(int i=0;i<len;i++)
+	{
 		printf("%d ",x[i]);
 	}
 	printf("\n");
 	return 0;
 }
-int main(int argc,char *argv[]){
+int main(int argc,char *argv[])
+{
 	int len=strlen(argv[1]);
 	int comma_count=0;
-	for(int i=0;i<len;++i){
-		if(argv[1][i]==','){
+	for(int i=0;i<len;++i)
+	{
+		if(argv[1][i]==',')
+		{
 			comma_count++;
 		}
 	}
 	int *a=(int*)malloc((comma_count+2)*4);
 	a[0]=atoi(&argv[1][0]);
 	int m=1;
-	for(int i=0;i<len;i++){
-		if(argv[1][i]==','){
+	for(int i=0;i<len;i++)
+	{
+		if(argv[1][i]==',')
+		{
 			a[m]=atoi(&argv[1][i+1]);
 			m++;
 		}
